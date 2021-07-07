959798c89b1e386747e282f798cac52c19a576a3
Aarav Singh <aaravzen@gmail.com>
2021-07-07 11:46:44 -0700

    fix image urls

--- scripts/generate_images.py
+++ scripts/generate_images.py
@@ -4,4 +4,4 @@ image_album = raw_input()
 path = 'static/images/%s' % image_album
 
 for image_name in os.listdir(path):
-    print("![%s](/images/%s/%s.jpg?resize=600 '%s')" % (image_name, image_album, image_name, image_name))
+    print("![%s](/images/%s/%s?resize=600 '%s')" % (image_name, image_album, image_name, image_name))
